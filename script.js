b0ad83a83e3faa9220a0856b1ab9cf7a804c3834
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-07-02 23:16:54 +0530

    Enable drag and drop functionality

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const addButton = document.querySelector(".add-button");
 const taskContainer = document.querySelectorAll(".task-cont");
 
 let tasks =  [];
-
+let draggedTask = null;
 render();
+
+
 addButton.addEventListener("click", () => {
+
   const taskName = prompt("Enter task name:");
 
   if (!taskName) return;
@@ -20,11 +23,38 @@ addButton.addEventListener("click", () => {
 
 
 function render(){
+ 
   tasks.forEach((taskData) => {
     const task = document.createElement("p");
     task.classList.add("task");
     task.textContent = taskData.text;
     task.draggable = true;
+
+    // allow dragging 
+    task.setAttribute("draggable", "true");
+
+    // start dragging
+    task.addEventListener("dragstart", () => {
+      draggedTask = taskData.id;
+    })
+
+    // finish dragging 
+    task.addEventListener("dragend", () => {
+      draggedTask = null;
+    })
     taskContainer[taskData.column].appendChild(task);
   })
 }
+
+taskContainer.forEach((container, index) => {
+ 
+
+  // drop
+  container.addEventListener("drop", ()=> {
+    const task = tasks.find((t) => t.id === draggedTask );
+
+    task.column = index;
+
+    
+  })
+})
